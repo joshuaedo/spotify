c1d47f484e7b2fe357b7d7bda5f62d464e7ac5c4
Joshua <joshua.edo01@gmail.com>
2023-02-20 10:08:09 +0100

    add sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
+import Sidebar from '@/components/Sidebar';
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <>
@@ -14,7 +9,11 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>Spotify is up!!!</h1>
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+      <div>{/* Player */}</div>
     </>
   );
 }
