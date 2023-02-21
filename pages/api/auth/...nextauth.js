16edfeec3d5bf9eab47df4ef2a07f2dd9987aab6
Joshua <joshua.edo01@gmail.com>
2023-02-21 08:56:13 +0100

    complete async jwt call

--- pages/api/auth/...nextauth.js
+++ pages/api/auth/...nextauth.js
@@ -1,7 +1,31 @@
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
+import spotifyApi, { LOGIN_URL } from '../../../lib/spotify';
 
-export const authOptions = {
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, // 1 hr returns as 3600 returns from spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken, // Replace if new one came back else fall back to old one
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
@@ -11,5 +35,33 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // on initial signIn
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccount,
+          accessTokenExpires: account.expires_at * 1000, // expiry time is being registered in milliseconds
+        };
+      }
+      // return back previous token if access token has not expired
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('access token is still valid');
+        return token;
+      }
+
+      // if access token has expired, it must be updated
+      // else {
+      console.log('access token has expired, refreshing...');
+      return await refreshAccessToken(token);
+      // }
+    },
+  },
+});
