11e9213fe8c4d1894cdb85aa0be2f67e9bc16fed
Joshua Edo <joshua.edo01@gmail.com>
2023-03-05 20:16:48 +0000

    Update 8 files

--- components/FullPlayer.js
+++ components/FullPlayer.js
@@ -1,27 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { currentTrackIdState } from '@/atom/songAtom';
-import useSpotify from '@/hooks/useSpotify';
-import { useCallback, useEffect, useState } from 'react';
-import { isPlayingState } from '@/atom/songAtom';
-import Image from 'next/image';
-import useSongInfo from '@/hooks/useSongInfo';
-import { playerIcons } from './playerIcons';
-import { ChevronDownIcon, PauseIcon, PlayIcon } from '@heroicons/react/outline';
-import { motion } from 'framer-motion';
-import { shuffle } from 'lodash';
-import { useRecoilValue, useRecoilState } from 'recoil';
-import { playlistIdState, playlistState } from '@/atom/playlistAtom';
+import { currentTrackIdState } from "@/atom/songAtom";
+import useSpotify from "@/hooks/useSpotify";
+import { useCallback, useEffect, useState } from "react";
+import { isPlayingState } from "@/atom/songAtom";
+import Image from "next/image";
+import useSongInfo from "@/hooks/useSongInfo";
+import { playerIcons } from "./playerIcons";
+import { ChevronDownIcon, PauseIcon, PlayIcon } from "@heroicons/react/outline";
+import { motion } from "framer-motion";
+import { shuffle } from "lodash";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { playlistIdState, playlistState } from "@/atom/playlistAtom";
 
 export default function FullPlayer(props) {
   const colors = [
-    'from-indigo-500',
-    'from-blue-500',
-    'from-green-500',
-    'from-yellow-500',
-    'from-purple-500',
-    'from-pink-500',
-    'from-orange-500',
-    'from-red-500',
+    "from-indigo-500",
+    "from-blue-500",
+    "from-green-500",
+    "from-yellow-500",
+    "from-purple-500",
+    "from-pink-500",
+    "from-orange-500",
+    "from-red-500",
   ];
   const [
     connectDevice,
@@ -65,10 +65,10 @@ export default function FullPlayer(props) {
   };
   const handleRepeat = () => {
     spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-      if (data.body?.repeat_state === 'off') {
-        spotifyApi.setRepeat('track');
+      if (data.body?.repeat_state === "off") {
+        spotifyApi.setRepeat("track");
       } else {
-        spotifyApi.setRepeat('off');
+        spotifyApi.setRepeat("off");
       }
     });
   };
@@ -76,29 +76,25 @@ export default function FullPlayer(props) {
     setColor(shuffle(colors).pop());
   }, [songInfo]);
 
- 
-
   return (
-    <div className='text-white flex-grow h-screen overflow-y-scroll scrollbar-hide bg-[#121212]'>
-      <header className='absolute inset-x-0 top-0 flex space-x-3 space-y-5 justify-evenly md:justify-start'>
-        <div className='flex '>
+    <div className="text-white flex-grow h-screen overflow-y-scroll scrollbar-hide bg-[#121212]">
+      <header className="absolute inset-x-0 top-0 flex space-x-3 space-y-5 justify-evenly md:justify-start">
+        <div className="flex ">
           <ChevronDownIcon
-            className='h-5 w-5 md:h-10 md:w-10 place-self-center cursor-pointer hover:opacity-75'
+            className="h-5 w-5 md:h-10 md:w-10 place-self-center cursor-pointer hover:opacity-75"
             onClick={props.toggleFullScreen}
-          />{' '}
+          />{" "}
         </div>
-        <div className='opacity-70 text-center md:text-start text-xs'>
-          <p className='font-bold inline-block'>
-            PLAYING FROM ALBUM
-          </p>
+        <div className="opacity-70 text-center md:text-start text-xs">
+          <p className="font-bold inline-block">PLAYING FROM ALBUM</p>
           <p> {songInfo?.album?.name}</p>
         </div>
 
-        <div className='flex p-1'>
+        <div className="flex p-1">
           <Image
             src={myLyrics}
-            alt='lyrics'
-            className='button cursor-not-allowed'
+            alt="lyrics"
+            className="button cursor-not-allowed"
           />
         </div>
       </header>
@@ -107,7 +103,7 @@ export default function FullPlayer(props) {
         className={`grid md:flex bg-gradient-to-b to-[#121212] ${color} text-white p-8 h-2/3 md:h-5/6`}
       >
         <motion.div
-          className='space-y-6 place-self-center md:place-self-end block md:flex mt-6 pt-6 md:mt-1 md:pt-1'
+          className="space-y-6 place-self-center md:place-self-end block md:flex mt-6 pt-6 md:mt-1 md:pt-1"
           initial={{
             opacity: 0,
             y: -50,
@@ -117,18 +113,18 @@ export default function FullPlayer(props) {
             y: 0,
           }}
         >
-          <div className=''>
+          <div className="">
             <Image
               src={songInfo?.album.images?.[0]?.url}
               alt={songInfo?.name}
               width={220}
               height={220}
-              className='h-72 w-72 md:h-36 md:w-36 shadow-2xl mr-0 md:mr-4 rounded '
+              className="h-72 w-72 md:h-36 md:w-36 shadow-2xl mr-0 md:mr-4 rounded "
             />
           </div>
-          <div className='font-bold md:place-self-center'>
-            <h2 className='text-2xl lg:text-5xl '>{songInfo?.name}</h2>
-            <p className='text-gray-400 text-clip text-md'>
+          <div className="font-bold md:place-self-center">
+            <h2 className="text-2xl lg:text-5xl ">{songInfo?.name}</h2>
+            <p className="text-gray-400 text-clip text-md">
               {songInfo?.artists[0]?.name}
             </p>
           </div>
@@ -136,41 +132,41 @@ export default function FullPlayer(props) {
       </section>
 
       <section>
-        <div className=''>
-          <div className='stay space-x-10 items-center justify-center p-3 m-1 mt-3'>
+        <div className="">
+          <div className="stay space-x-10 items-center justify-center p-3 m-1 mt-3">
             <Image
               src={shuffleIcon}
-              alt='shuffle'
-              className='fullscreen-button'
+              alt="shuffle"
+              className="w-5 h-4"
               onClick={handleShuffle}
             />
             <Image
               src={previous}
-              alt='previous'
-              className='fullscreen-button'
+              alt="previous"
+              className="fullscreen-button"
               onClick={() => spotifyApi.skipToPrevious()}
             />
             {isPlaying ? (
               <PauseIcon
                 onClick={handlePlayPause}
-                className='cursor-pointer hover:opacity-75 w-16 h-16'
+                className="cursor-pointer hover:opacity-75 w-16 h-16"
               />
             ) : (
               <PlayIcon
                 onClick={handlePlayPause}
-                className='cursor-pointer hover:opacity-75 w-16 h-16'
+                className="cursor-pointer hover:opacity-75 w-16 h-16"
               />
             )}
             <Image
               src={next}
-              alt='next'
-              className='fullscreen-button'
+              alt="next"
+              className="fullscreen-button"
               onClick={() => spotifyApi.skipToNext()}
             />
             <Image
               src={repeat}
-              alt='repeat'
-              className='fullscreen-button'
+              alt="repeat"
+              className="w-5 h-4"
               onClick={handleRepeat}
             />
           </div>
--- components/MediaShare.js
+++ components/MediaShare.js
@@ -9,7 +9,7 @@ import {
   TumblrShareButton,
   TwitterShareButton,
   WhatsappShareButton,
-} from 'react-share';
+} from "react-share";
 
 // Icons
 import {
@@ -22,53 +22,57 @@ import {
   TumblrIcon,
   TwitterIcon,
   WhatsappIcon,
-} from 'react-share';
+} from "react-share";
 
-import { ChevronDownIcon } from '@heroicons/react/outline';
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
 const shareUrl = `https://joshuaedo-spotify.vercel.app`;
 
 export default function MediaShare(props) {
   return (
     <>
-      <div className='justify-center items-center space-x-3 space-y-3 inline lg:flex '>
-        <div className='justify-center items-center text-center  pt-6 md:pt-2 text-white absolute bottom-0 transparent rounded-md'>
+      <div className="justify-center items-center space-x-3 space-y-3 inline lg:flex ">
+        <div className="justify-center items-center text-center  pt-6 md:pt-2 text-white absolute bottom-0 transparent rounded-md">
           <div>
             <ChevronDownIcon
-              className='h-10 w-10 text-white p-1 m-3 md:m-5 animate-pulse cursor-pointer'
+              className="h-10 w-10 text-white p-1 m-3 md:m-5 animate-pulse cursor-pointer"
               onClick={props.toggleMediaShare}
             />
           </div>
           <div>
-            <p className='text-2xl md:text-5xl mb-3 md:mb-12 font-semibold'>
+            <p className="text-2xl md:text-5xl mb-3 md:mb-12 font-semibold">
               Share a song?
             </p>
             <EmailShareButton url={shareUrl}>
-              <EmailIcon size={102} round className='' />
+              <EmailIcon size={102} round className="" />
             </EmailShareButton>
             <TumblrShareButton url={shareUrl}>
-              <TumblrIcon size={102} round className='' />
+              <TumblrIcon size={102} round className="" />
             </TumblrShareButton>
             <FacebookShareButton url={shareUrl}>
-              <FacebookIcon size={102} round className='' />
+              <FacebookIcon size={102} round className="" />
             </FacebookShareButton>
             <LinkedinShareButton url={shareUrl}>
-              <LinkedinIcon size={102} round className='' />
+              <LinkedinIcon size={102} round className="" />
             </LinkedinShareButton>
             <TwitterShareButton url={shareUrl}>
-              <TwitterIcon size={102} round className='animate-bounce' />
+              <TwitterIcon
+                size={102}
+                round
+                className="animate-bounce temporary-bounce"
+              />
             </TwitterShareButton>
             <WhatsappShareButton url={shareUrl}>
-              <WhatsappIcon size={102} round className='' />
+              <WhatsappIcon size={102} round className="" />
             </WhatsappShareButton>
             <PinterestShareButton url={shareUrl}>
-              <PinterestIcon size={102} round className='' />
+              <PinterestIcon size={102} round className="" />
             </PinterestShareButton>
             <RedditShareButton url={shareUrl}>
-              <RedditIcon size={102} round className='' />
+              <RedditIcon size={102} round className="" />
             </RedditShareButton>
             <TelegramShareButton url={shareUrl}>
-              <TelegramIcon size={102} round className='' />
+              <TelegramIcon size={102} round className="" />
             </TelegramShareButton>
           </div>
         </div>
